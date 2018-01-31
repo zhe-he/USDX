6626a01ad68a17a4e6bfcb285054c98c4c9a2d25
zhe-he <hezhe@ihangmei.com>
2018-01-31 09:46:32 +0800

    add

--- src/views/aboutusdx.vue
+++ src/views/aboutusdx.vue
@@ -8,31 +8,39 @@
         </h2>
 
         <div :key="index" v-show="cur==index" v-for="(item,index) in aboutusdx" class="aboutContent" :class="{'phaseOneBox':index==0,'phaseTwoBox':index==1,'phaseThreeBox':index==2}">
-            <div class="content">
-                <h2 v-html="item.h2"></h2>
-                <div class='line'></div>
-                <p v-for="i in item.p" v-html="i"></p>
-                <ul v-if="item.ul" class='phaseTwoDesc'>
-                    <li :class="i.class" v-html="i.li" v-for="i in item.ul"></li>
-                </ul>
-                <div class="order">
-                    <a href="javascript:;" @click="run(-1)" class="swiperBtn swiperLeft mobileHide" :class="{'active':cur != 0}">
-                        <i class="icon iconfont icon-left_arrow"></i>
-                    </a>
-                        <div class="number">
-                            <i class="icon iconfont big_number_font" :class="{'icon-one':index==0,'icon-two':index==1,'icon-three':index==2}"></i>
-                            &nbsp;<span class="small">/ </span><i class="small_number_font icon iconfont icon-three"></i>
-                        </div>
-                    <a href="javascript:;" @click="run(1)" class="swiperBtn swiperRight mobileHide" :class="{'active':cur != 2}">
-                        <i class="icon iconfont icon-right_arrow"></i>
-                    </a>
+            <transition name="fade">
+                <div v-show="cur==index" class="content">
+                    <h2 v-html="item.h2"></h2>
+                    <div class='line'></div>
+                    <p v-for="i in item.p" v-html="i"></p>
+                    <ul v-if="item.ul" class='phaseTwoDesc'>
+                        <li :class="i.class" v-html="i.li" v-for="i in item.ul"></li>
+                    </ul>
+                    <div class="order">
+                        <a href="javascript:;" @click="run(-1)" class="swiperBtn swiperLeft mobileHide" :class="{'active':cur != 0}">
+                            <i class="icon iconfont icon-left_arrow"></i>
+                        </a>
+                            <div class="number">
+                                <i class="icon iconfont big_number_font" :class="{'icon-one':index==0,'icon-two':index==1,'icon-three':index==2}"></i>
+                                &nbsp;<span class="small">/ </span><i class="small_number_font icon iconfont icon-three"></i>
+                            </div>
+                        <a href="javascript:;" @click="run(1)" class="swiperBtn swiperRight mobileHide" :class="{'active':cur != 2}">
+                            <i class="icon iconfont icon-right_arrow"></i>
+                        </a>
+                    </div>
                 </div>
-            </div>
+            </transition>
             <div class="rightPicBox">
                 <div class="bottomBase"></div>
-                <div v-show="cur==0" class="topImage"></div>
-                <div v-show="cur==1" class="topTwoImage"></div>
-                <div v-show="cur==2" class="topThreeImage"></div>
+                <transition name="mvleft">
+                    <div v-show="cur==0" class="topImage"></div>
+                </transition>
+                <transition name="mvleft">
+                    <div v-show="cur==1" class="topTwoImage"></div>
+                </transition>
+                <transition name="mvleft">
+                    <div v-show="cur==2" class="topThreeImage"></div>
+                </transition>
             </div>
         </div>
     </div>
@@ -61,5 +69,17 @@
 </script>
 
 <style lang="scss">
-    
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .6s;
+    }
+    .fade-enter, .fade-leave-active {
+        opacity: 0;
+    }
+    .mvleft-enter-active, .mvleft-leave-active {
+        transition: all .6s;
+    }
+    .mvleft-enter, .mvleft-leave-active {
+        transform: translate3d(50%,0,0);
+        opacity: 0
+    }
 </style>
